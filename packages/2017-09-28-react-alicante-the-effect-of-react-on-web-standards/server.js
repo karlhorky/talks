0d339cbeb5b2c55078775a2fad64e011a2985c9b
Karl Horky <karl.horky@gmail.com>
2017-08-23 01:07:07 +0200

    Change server port

--- packages/2017-09-28-react-alicante-the-effect-of-react-on-web-standards/server.js
+++ packages/2017-09-28-react-alicante-the-effect-of-react-on-web-standards/server.js
@@ -8,12 +8,14 @@ var config = require("./webpack.config");
 var app = express();
 var compiler = webpack(config);
 
-var serverPort = process.env.PORT || 3000;
+var serverPort = process.env.PORT || 8000;
 
-app.use(require("webpack-dev-middleware")(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
 
 app.use(require("webpack-hot-middleware")(compiler));
 
@@ -21,7 +23,7 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(serverPort, "localhost", function (err) {
+app.listen(serverPort, "localhost", function(err) {
   if (err) {
     console.log(err);
     return;
