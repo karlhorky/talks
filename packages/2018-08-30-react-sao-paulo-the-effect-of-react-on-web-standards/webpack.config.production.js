6aa7962633a61dbdf7d59e8d651b6e661bd8c651
Karl Horky <karl.horky@gmail.com>
2018-08-30 17:40:25 -0300

    Fix image

--- packages/2018-08-30-react-sao-paulo-the-effect-of-react-on-web-standards/webpack.config.production.js
+++ packages/2018-08-30-react-sao-paulo-the-effect-of-react-on-web-standards/webpack.config.production.js
@@ -1,47 +1,51 @@
 /* eslint-disable */
 
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    "babel-polyfill",
-    "./index"
-  ],
+  entry: ['babel-polyfill', './index'],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath:
+      '/talks/packages/2018-08-30-react-sao-paulo-the-effect-of-react-on-web-standards/dist/',
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        "NODE_ENV": JSON.stringify("production")
-      }
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false
-      }
-    })
+        warnings: false,
+      },
+    }),
   ],
   module: {
-    loaders: [{
-      test: /\.md$/,
-      loader: "html-loader!markdown-loader?gfm=false"
-    }, {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(png|jpg|gif)$/,
-      loader: "url-loader?limit=8192"
-    }, {
-      test: /\.svg$/,
-      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-    }]
-  }
+    loaders: [
+      {
+        test: /\.md$/,
+        loader: 'html-loader!markdown-loader?gfm=false',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=8192',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
 };
